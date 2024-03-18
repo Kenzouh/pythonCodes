151adb46075ace969229603f37b760f53e7d6512
Kenzo Ragundiaz（ラグンディアズ、賢三） <144200593+Kenzouh@users.noreply.github.com>
2024-03-18 15:30:50 +0800

    Add files via upload

---
+++ pyAss1_Print1.py
@@ -0,0 +1,19 @@
+print("==========================")
+print("|| Python Assignment #2 ||")
+print("||   Print Statement 1  ||")
+print("==========================")
+
+print("\nSubmitted to:      Dr. Aleta Fabregas")
+print("Submitted by:      Kenzo C. Ragundiaz")
+print("                   (BSIT 2-5)")
+print("Date submitted:    March 15, 2024 (Friday) ")
+
+print("\n=================================\n")
+
+print("Hi! My name is Kenzo C. Ragundiaz!")
+print("I am 20 years old.")
+print("My birthday is on July 14, 2003!")
+print("I study in PUP, and I'm from BSIT 2-5!")
+
+print("\nNice to meet you! :D")
+
