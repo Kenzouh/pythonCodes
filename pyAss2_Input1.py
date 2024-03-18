995c6e9b6a8b3627cba3acb0e26037bdfc36ce35
Kenzo Ragundiaz（ラグンディアズ、賢三） <144200593+Kenzouh@users.noreply.github.com>
2024-03-18 15:31:32 +0800

    Add files via upload

---
+++ pyAss2_Input1.py
@@ -0,0 +1,24 @@
+print("==========================")
+print("|| Python Assignment #2 ||")
+print("||     User Input 1     ||")
+print("==========================")
+
+print("\nSubmitted to:      Dr. Aleta Fabregas")
+print("Submitted by:      Kenzo C. Ragundiaz")
+print("                   (BSIT 2-5)")
+print("Date submitted:    March 15, 2024 (Friday) ")
+
+print("\n=================================\n")
+
+print("Hi! My name is Kenzo! 20 years old, I like making friends!")
+
+name = input("\nYour name:\t")
+age = int(input("Your age:\t"))
+hobby = input("Your hobby:\t")
+
+print(f"\nHi, {name}!")
+print(f"You're {age} years old!")
+print(f"You like {hobby}.")
+
+print(f"\nNice to meet you, {name}! :D")
+
---
+++ pyAss2_Input2.py
@@ -0,0 +1,24 @@
+print("==========================")
+print("|| Python Assignment #2 ||")
+print("||     User Input 2     ||")
+print("==========================")
+
+print("\nSubmitted to:      Dr. Aleta Fabregas")
+print("Submitted by:      Kenzo C. Ragundiaz")
+print("                   (BSIT 2-5)")
+print("Date submitted:    March 15, 2024 (Friday) ")
+
+print("\n=================================\n")
+
+print("Welcome to Notes!\n")
+
+dream = input("My dream is to be a/an: ")
+faveFood = input("My favorite food is: ")
+faveAnimal = input("My favorite animal: ")
+faveShow = input("My favorite show: ")
+faveGame = input("My favorite game: ")
+
+print(f"\nYour dream is to become a/an {dream}!")
+print(f"You like eating {faveFood}!")
+print(f"You want to have a {faveAnimal} as a pet!")
+print(f"You love watching {faveShow} and you love playing {faveGame}!")
